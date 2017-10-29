c7c4b6f1c20b098581ff6a25242263e0291972ca
Jessica Greene <jessica0greene@gmail.com>
2017-10-29 20:37:46 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,6 +4,9 @@ def position_taken?(board, index)
     return false
   elsif board[index] == ""
     return false
-
+  elsif board[index] == nil
+    return false
+  elsif (board[index] == "X") || (board[index] == "O")
+    return true
   end
 end
